1a32acd1a783f925567617c6ef231c3e7c47a8e8
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-07-10 14:38:56 +0530

    Service added

--- src/main/java/com/LavaCandy/Personal/Task/Manager/service/UserService.java
+++ src/main/java/com/LavaCandy/Personal/Task/Manager/service/UserService.java
@@ -1,5 +1,45 @@
 package com.LavaCandy.Personal.Task.Manager.service;
 
+import com.LavaCandy.Personal.Task.Manager.model.User;
+import com.LavaCandy.Personal.Task.Manager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    public User creatUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User updateUser(Long id, User user) {
+        Optional<User> existingUser = userRepository.findById(id);
+        if(existingUser.isPresent()) {
+            User updatedUser = existingUser.get();
+            updatedUser.setName(user.getName());
+            updatedUser.setEmail(user.getEmail());
+            updatedUser.setPassword(user.getPassword());
+            return userRepository.save(updatedUser);
+        }
+        return null;
+    }
     
 }
