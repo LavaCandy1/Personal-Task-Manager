371b38246f855ec1de42d06261de0ca62b407117
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-08-29 14:16:18 +0530

    Removed Boards from responseDTO in get all users

--- src/main/java/com/LavaCandy/Personal/Task/Manager/dto/UserResponseDTO.java
+++ src/main/java/com/LavaCandy/Personal/Task/Manager/dto/UserResponseDTO.java
@@ -2,21 +2,16 @@ package com.LavaCandy.Personal.Task.Manager.dto;
 
 import com.LavaCandy.Personal.Task.Manager.model.User;
 
-import java.util.List;
-
-import com.LavaCandy.Personal.Task.Manager.model.Board;
 
 public class UserResponseDTO {
     private Long id;
     private String name;
     private String email;
-    private List<Board> boards;
 
     public UserResponseDTO(User user) {
         this.id = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
-        this.boards = user.getBoards();
     }
 
     // getters
@@ -28,8 +23,5 @@ public class UserResponseDTO {
     }
     public String getEmail() {
         return email;
-    }
-    public List<Board> getBoard() {
-        return boards;
     }
 }
