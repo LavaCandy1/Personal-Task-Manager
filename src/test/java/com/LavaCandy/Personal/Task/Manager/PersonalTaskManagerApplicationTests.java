9d39741d75d1bcc70fc2825e7ab78d8b62a97e86
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-08-30 15:38:28 +0530

    added test profile for github CI

--- src/test/java/com/LavaCandy/Personal/Task/Manager/PersonalTaskManagerApplicationTests.java
+++ src/test/java/com/LavaCandy/Personal/Task/Manager/PersonalTaskManagerApplicationTests.java
@@ -2,8 +2,10 @@ package com.LavaCandy.Personal.Task.Manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PersonalTaskManagerApplicationTests {
 
 	@Test
